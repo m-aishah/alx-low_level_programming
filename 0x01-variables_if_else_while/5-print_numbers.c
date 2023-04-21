dce7d4e448d4180336d49dcb8ccf9c0e829f4f14
Aishah <maishah2540@gmail.com>
2023-04-21 14:02:17 -0700

    Checking with checker

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,9 +4,10 @@
  * A program to print all single digits
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
 	printf("\n");
