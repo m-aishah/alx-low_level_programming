d83fca6003fbe148d63762dad0357ad54afe321a
Aishah <maishah2540@gmail.com>
2023-04-21 00:11:48 -0700

    Task 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * A program to print all numbers of base 16
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10 ; n++)
+		putchar((n%10) + '0');
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
