1d41e129cbc3ec2ddf93349d01acb4958f940893
Aishah <maishah2540@gmail.com>
2023-04-21 14:20:04 -0700

    Checking with checker

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,13 +4,13 @@
  * A program to print all numbers of base 16
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
 	char c;
 
 	for (n = 0; n < 10 ; n++)
-		putchar((n%10) + '0');
+		putchar((n % 10) + '0');
 	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
 	putchar('\n');
