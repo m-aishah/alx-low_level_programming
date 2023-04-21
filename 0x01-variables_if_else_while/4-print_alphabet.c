3926f788cec3a92a9904fc72bf59e569c6252193
Aishah <maishah2540@gmail.com>
2023-04-20 23:32:41 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * A program to print all lower case alphabets except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch ++)
+	{
+		if ((ch != 'q') && (ch != 'e'))
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
