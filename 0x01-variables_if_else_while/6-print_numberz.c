06c915af1d33918fc5da067cbdef362945e4b772
Aishah <maishah2540@gmail.com>
2023-04-21 14:07:44 -0700

    Checking with checker

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,9 +4,10 @@
  * A program to print digits using putchar
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
+
 	for (n = 48 ; n <= 57; n++)
 		putchar(n);
 	putchar('\n');
