d1ded0750fc7e6935655aa247cb9bac61df5e44f
Aishah <maishah2540@gmail.com>
2023-04-21 14:23:17 -0700

    Checking with checker

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,9 +4,10 @@
  * A program to print all possible combinations of single digit numbers
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
+
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
