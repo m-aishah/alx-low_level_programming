6625f72a9461a5cf85da519718bb4da45f7aa7ea
Aishah <maishah2540@gmail.com>
2023-04-20 23:47:01 -0700

    Task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * A program to print digits using putchar
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int n;
+	for (n = 48 ; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
