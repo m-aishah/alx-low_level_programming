cfa88eb878cf6910f0f3952db5a0730b8e411d1c
Aishah <maishah2540@gmail.com>
2023-04-20 23:38:26 -0700

    Task 4. When I was having the alphabet soup, I never thought it would pay off

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * A program to print all single digits
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int n;
+	for (n = 0; n < 11; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
