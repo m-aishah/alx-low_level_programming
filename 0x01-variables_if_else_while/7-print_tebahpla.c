a23144eda6a7c275bc8134e23939d7eb33078b84
Aishah <maishah2540@gmail.com>
2023-04-21 14:14:29 -0700

    Checking with checker

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,9 +4,10 @@
  * A program to print lowercase alphabets in reverse
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char ch;
+	
 	for (ch = 122; ch >= 97; ch--)
 		putchar(ch);
 	putchar('\n');
