2569e8058c1a296cb721449c4fc81646e9985b0f
Aishah <maishah2540@gmail.com>
2023-04-21 13:46:06 -0700

    Checking with checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- *main - Entry point
+/**
+ * main - Entry point. Print the last digit of a randomly generated number
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
