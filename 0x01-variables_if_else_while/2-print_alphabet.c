8c69820be0fea4885be6518ee019729ac992aa62
Aishah <maishah2540@gmail.com>
2023-04-21 13:52:55 -0700

    Checking with checker

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,13 @@
  * main - Entry point
  * program to print lowercase alphabets
  * Return: Always 0 (Success)
- */ 
- int main(void)
+ */
+int main(void)
 {
-	 char ch;
-	 for (ch = 'a' ; ch <= 'z' ; ch++)
-		 putchar(ch);
-	 putchar('\n');
-	 return (0);
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
