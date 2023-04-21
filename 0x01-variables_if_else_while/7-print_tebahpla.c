4921275aeed7a23f5f68d0685f4cfe7b7e91b7c9
Aishah <maishah2540@gmail.com>
2023-04-21 00:02:59 -0700

    Task 7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * A program to print lowercase alphabets in reverse
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char ch;
+	for (ch = 122; ch >= 97; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
