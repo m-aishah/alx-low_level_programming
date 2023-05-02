9634ace8595690bbab2f6c4dfadb5002f307d421
Aishah <maishah2540@gmail.com>
2023-05-02 12:00:34 -0700

    Task 3.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Function to print a string followed by a new line
+ * @str: Pointer to a string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str++)
+		_putchar(*str);
+	_putchar('\n');
+}
