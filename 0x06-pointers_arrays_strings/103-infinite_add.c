7b24d946de1e1c166fd50538bad35864156a4d07
Aishah <maishah2540@gmail.com>
2023-05-04 16:52:32 -0700

    Task 10.

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * infinite_add - Program to add 2 numbers
+ * @n1: First Number
+ * @n2: Second Number
+ * @r: Buffer to store result
+ * @size_r: Buffer size
+ * Return: character pointer to result (Sucess), else return 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
