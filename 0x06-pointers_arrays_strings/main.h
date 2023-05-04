18bf579094c16c660af4d086e1d39a93865bafa6
Aishah <maishah2540@gmail.com>
2023-05-04 14:34:18 -0700

    Task 8.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+
 #endif
