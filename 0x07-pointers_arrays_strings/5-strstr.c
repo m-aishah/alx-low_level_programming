b9f5f16acb7d45b0af059685394ddad403f3c9c7
Aishah <maishah2540@gmail.com>
2023-05-08 12:16:26 -0700

    Task 5.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,6 +12,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int start, j, i = 0;
 
+	if (*needle == 0)
+		return (haystack);
+
 	while (*(haystack + i) != '\0')
 	{
 		if (haystack[i] == *needle)
