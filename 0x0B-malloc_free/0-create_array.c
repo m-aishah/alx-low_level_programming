f3a441bc46a64e41a5928a90e9bf792fbaf4840f
Aishah <maishah2540@gmail.com>
2023-05-15 01:20:52 -0700

    Task 0.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,18 +15,19 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char *)malloc((sizeof(char) * size) + 1);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = (char *)malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	arr[size] = '\0';
-
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return (arr);
 }
-
-
