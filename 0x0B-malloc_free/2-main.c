59902e9e612d35ab42ca3aee6bb4bcbd318aec7b
Aishah <maishah2540@gmail.com>
2023-05-15 15:27:07 -0700

    Corrected Task 2.

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -9,15 +9,29 @@
  */
 int main(void)
 {
-    char *s;
+    char *s1, *s2, *s3;
 
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
+    s1 = str_concat("Hello", NULL);
+    s2 = str_concat(NULL, "Hello");
+    s3 = str_concat(NULL, NULL);
+    if (s1 == NULL)
     {
         printf("failed\n");
         return (1);
     }
-    printf("%s\n", s);
-    free(s);
+    if (s1 == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    if (s1 == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n%s\n%s", s1, s2, s3);
+    free(s1);
+    free(s2);
+    free(s3);
     return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
-/**
- * concatenate - Function to concatenate two strings
- * @s1: The first string
- * @s2: The second string
- *
- * Return: A pointer to the concatenated string
- */
-
-char *concatenate(char *s1, char *s2)
-{
-	int i, l1 = 0, l2 = 0, j = 0;
-	char *s3;
-
-	for (i = 0; s1[i]; i++)
-		l1++;
-	if (s2 == NULL)
-	{
-		s3 = (char *)malloc((sizeof(char) * l1) + 1);
-		for (i = 0; i < l1; i++)
-			s3[i] = s1[i];
-		s3[i] = '\0';
-		return (s3);
-	}
-	for (i = 0; s2[i]; i++)
-		l2++;
-
-	s3 = (char *)malloc((sizeof(char) * (l1 + l2)) + 1);
-
-	if (s3 == NULL)
-		return (NULL);
-
-	for (i = 0; i < l1; i++)
-		s3[i] = s1[i];
-	for (i = l1; i < (l1 + l2); i++)
-	{
-		s3[i] = s2[j];
-		j++;
-	}
-	s3[i] = '\0';
-
-	return (s3);
-}
 /**
  * str_concat - Function that concatenates 2 strings
  * @s1: The first string
@@ -54,32 +11,27 @@ char *concatenate(char *s1, char *s2)
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, l2 = 0;
+	int i, j = 0, l = 0;
 	char *s3;
 
 	if (s1 == NULL)
-	{
-		if (s2 == NULL)
-		{
-			s3 = (char *)malloc(1);
-			*s3 = '\0';
-		}
-		for (i = 0; s2[i]; i++)
-			l2++;
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-		s3 = (char *)malloc((sizeof(char) * l2) + 1);
+	for (i = 0; s1[i] || s2[i]; i++)
+		l++;
 
-		if (s3 == NULL)
-			return (NULL);
-		for (i = 0; i < l2; i++)
-			s3[i] = s2[i];
-		s3[i] = '\0';
+	s3 = (char *)malloc(sizeof(char) * l);
 
-		return (s3);
+	if (s3 == NULL)
+		return (NULL);
 
-	}
+	for (i = 0; s1[i]; i++)
+		s3[j++] = s1[i];
 
-	s3 = concatenate(s1, s2);
+	for (i = 0; s2[i]; i++)
+		s3[j++] = s2[i];
 
 	return (s3);
 }
