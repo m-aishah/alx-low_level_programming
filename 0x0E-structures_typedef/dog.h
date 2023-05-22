e7b60aaea51d502088cdbe89054172d71ddb2e17
Aishah <maishah2540@gmail.com>
2023-05-22 09:25:25 -0700

    Task 0.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: A structure called dog that contains some info about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
