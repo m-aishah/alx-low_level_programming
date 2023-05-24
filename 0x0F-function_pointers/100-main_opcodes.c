28e9af82f7e64032c2dfc1b50456702addc70ba5
Aishah <maishah2540@gmail.com>
2023-05-24 12:20:23 -0700

    Task 5.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program to print the opcodes of its own function
+ * @argc: The number of arguments used to run the program
+ * @argv: A vector of the arguments used to run the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == -1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
