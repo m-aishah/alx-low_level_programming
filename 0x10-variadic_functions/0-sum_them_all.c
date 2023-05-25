b605c6fb2e8ad8449e7389dc208b34c9c714deaf
Aishah <maishah2540@gmail.com>
2023-05-25 04:53:29 -0700

    task 0.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,11 +14,14 @@ int sum_them_all(const unsigned int n, ...)
 	va_list numbers;
 	unsigned int index, sum = 0;
 
+	if (n == 0)
+		return (0);
+
 	va_start(numbers, n);
 
 	for (index = 0; index < n; index++)
 		sum += va_arg(numbers, int);
-	va_end(nums);
+	va_end(numbers);
 
 	return (sum);
 }
