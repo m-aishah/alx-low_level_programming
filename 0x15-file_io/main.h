4e8a5986e2a95a6c752bfa6ce711ae527a3e3cbd
Aishah <maishah2540@gmail.com>
2023-06-28 22:57:35 -0700

    Advanced Task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
