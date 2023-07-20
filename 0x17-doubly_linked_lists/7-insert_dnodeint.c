70f830c76692c6ea85276a03b9833076913509ba
Aishah <maishah2540@gmail.com>
2023-07-20 16:26:38 -0700

    Task 7. I redid using a different approach

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * insert_dnodeint_at_index - A function that insertse
- *		a new node at a given position of a dlistint_t list.
+ *		a newNode node at a given position of a dlistint_t list.
  * @h: A double pointer to the head of a doubly linked list.
  * @idx: The index at which the new node is to be inserted.
  * @n: Value of the new node to be inserted.
@@ -12,36 +12,30 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode, *traverse;
-	unsigned int i = 0;
+	dlistint_t *traverse = *h, *newNode;
 
-	if (idx == 0 || *h == NULL)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		traverse = traverse->next;
+		if (traverse == NULL)
+			return (NULL);
+	}
+
+	if (traverse->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
 
 	newNode->n = n;
+	newNode->prev = traverse;
+	newNode->next = traverse->next;
+	traverse->next->prev = newNode;
+	traverse->next = newNode;
 
-	traverse = *h;
-	while (traverse->next != NULL)
-	{
-		if (idx == i)
-		{
-			newNode->prev = traverse->prev;
-			newNode->next = traverse;
-			traverse->prev->next = newNode;
-			traverse->prev = newNode;
-			return (newNode);
-		}
-		traverse = traverse->next;
-		i++;
-	}
-	if (idx == i)
-		return (add_dnodeint_end(h, n));
-
-	free(newNode);
-	return (NULL);
+	return (newNode);
 }
-
