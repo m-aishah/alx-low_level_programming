f98b20e1f96a928906cab284950a5c9b6348ade5
Aishah <maishah2540@gmail.com>
2023-07-24 14:19:35 -0700

    Task 2.

---
+++ 0x18-dynamic_libraries/100-calculator.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - A functon that adds 2 integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The sum of the two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function that returns the difference between two numbers
+ * @a: The left operand
+ * @b: The right operand
+ * Return: The difference between the two integers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function to return the product of two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - A function that divides two integers
+ * @a: The numerator
+ * @b: The denominator
+ * Return: The result of dividing the 2 integers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - A function that returns the modulus of two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The modulus of the two integers
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
