6499bb893e3362dfc71deffa088918963d2764e4
Aishah <maishah2540@gmail.com>
2023-07-24 14:56:06 -0700

    Task 3.

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_PRELOAD=/root/alx-low_level_programming/0x18-dynamic_libraries/custom_gm.so
+./gm 9 8 10 24 75 9
---
+++ 0x18-dynamic_libraries/custom_gm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * lottery_draw: Function to overide the lottery draw
+ * Return: void
+ */
+
+void lottery_draw()
+{
+	printf("--> Please make me win!");
+}
