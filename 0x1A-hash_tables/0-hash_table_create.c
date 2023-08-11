e5ee912b4e598dc35905d8c267f7b65ad76f4c7b
Aishah <maishah2540@gmail.com>
2023-08-11 06:50:06 -0700

    Removed comments

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,17 +13,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	/*Allocate memory for hash table*/
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	/*Allocate memory for the hash_table array*/
 	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
-	/*Initialize all elements of hash_table array to NULL*/
+	
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
