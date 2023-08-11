3d3cd7dc8d7a8a09ac7e5052bafd439ca9b793a8
Aishah <maishah2540@gmail.com>
2023-08-11 07:49:51 -0700

    Fixed betty error for task 0.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,15 +13,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
+	/*Allocate memory for hash table*/
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
+	/*Allocate memory for hash table array*/
 	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 
+	/*Initialize all elements of hash table array to NULL*/
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
