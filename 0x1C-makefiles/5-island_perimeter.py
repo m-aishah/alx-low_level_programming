9c3f68cb1881ca6e26ff2a1f400deb8f6424cc6c
Aishah <maishah2540@gmail.com>
2023-09-28 13:34:49 -0700

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Defines a function that returns the perimeter of an island.'''
 
+
 def island_perimeter(grid):
     '''Returns the perimeter of an island.
 
@@ -14,13 +15,13 @@ def island_perimeter(grid):
     perimeter = 0
 
     if grid is not None:
-        for i in range (len(grid)):
-            for j in range (len(grid[0])):
-                if grid[i][j] == 1:
-                    if (grid[i][j + 1] == 1 or grid[i][j-1] == 1
-                            or grid[i + 1][j] == 1 or grid[i -1][j] ==1):
-                        perimeter += 2
-                    else:
-                        return 0
-        return perimeter + 2
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    perimeter += 4
+
+                    if row > 0 and grid[row - 1][col] == 1:
+                        perimeter -= 2
+                    if col > 0 and grid[row][col - 1] == 1:
+                        perimeter -= 2
     return perimeter
